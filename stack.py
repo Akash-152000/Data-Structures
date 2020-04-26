07d43262de5a4fd1ff39da3ff2825a5c6d8395b0
Akash <57212963+Akash-152000@users.noreply.github.com>
2020-04-26 10:38:45 +0530

    infixTopostfix using stack data structure

--- stack.py
+++ stack.py
@@ -1,35 +1,54 @@
 class stack:
     def __init__(self):
         self.stk=[]
-    def push(self,x):
-        self.stk.append(x)
+    def push(self,n):
+        self.stk.append(n)
     def pop(self):
-        if len(self.stk)==0:
-            return -1
-        else:
-            self.stk.pop()
-    def peep(self):
-        n=len(self.stk)
-        return self.stk[n-1]
-    def display(self):
-        return self.stk
+        a=self.stk[-1]
+        self.stk.pop()
+        return a
+
+di={"/":4,"*":3,"+":2,"-":1,"(":0}
+
 
 st=stack()
+answer=[]
+exp=input()
+exp1=list(exp)
+for ele in exp1:
+    if ele=="(":
+        st.push(ele)
+    elif ele in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        answer.append(ele)
+    elif ele in "*/+-":
+        if len(st.stk)==0:
+            st.push(ele)
+        elif di[st.stk[-1]]>di[ele]:
+            answer.append(st.pop())
+            st.push(ele)
+        else:
+            st.push(ele)
+    elif ele==")":
+        if st.stk[-1]!=")":
+            answer.append(st.pop())
+        else:
+            st.pop()
+
 while True:
-    print("1:PUSH")
-    print("2:POP")
-    print("3:PEEP")
-    print("4:DISPLAY")
-    print("5:EXIT")
-    ch=int(input())
-    if ch==1:
-        num=int(input("Enter the number to be pushed:"))
-        st.push(num)
-    elif ch==2:
-        st.pop()
-    elif ch==3:
-        print(st.peep())
-    elif ch==4:
-        print(st.display())
-    else:
+    if len(st.stk)==0:
         break
+    else:
+        answer.append(st.pop())
+str1=""
+for ele in answer:
+    if ele=="(":
+        pass
+    else:
+        str1+=ele
+print(str1)
+
+                      
+                      
+                      
+
+            
