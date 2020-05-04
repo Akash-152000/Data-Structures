2420e81508f12c7f78730e97ec4acabeb520a351
Akash <57212963+Akash-152000@users.noreply.github.com>
2020-05-04 11:31:37 +0530

    sumOfEdges

--- BinaryTree.py
+++ BinaryTree.py
@@ -16,8 +16,14 @@ class Binary_tree:
         elif traverse=="Levelorder":
             qu=[bt.root]
             return self.levelorder(qu,"")
+        elif traverse=="ReverseLevelOrder":
+            qu=[bt.root]
+            return self.reverseLevel(qu,"")
+        elif traverse=="SumOfEdges":
+            sum=0
+            return self.sum_edges(bt.root,sum)
         else:
-            return(self.postorder(bt.root,""))
+            return (self.postorder(bt.root,""))
 
     def preorder(self,start,traversal):
         """root<left<right"""
@@ -43,7 +49,7 @@ class Binary_tree:
             traversal+=str(start.data)+"-"
         return traversal
     
-     def levelorder(self,qu,traversal):
+    def levelorder(self,qu,traversal):
         while len(qu)!=0:
             m=qu.pop()
             traversal+=str(m.data)+"-"
@@ -52,7 +58,24 @@ class Binary_tree:
             if m.right!=None:
                 qu.insert(0,m.right)
         return traversal
-            
+    
+    def reverseLevel(self,qu,traversal):
+        while len(qu)!=0:
+            m=qu.pop()
+            traversal=str(m.data)+"-"+traversal
+            if m.left!=None:
+                qu.insert(0,m.left)
+            if m.right!=None:
+                qu.insert(0,m.right)
+        return traversal
+         
+    def sum_edges(self,start,sum):
+        """root<left<right"""
+        if start!=None:
+            sum+=start.data
+            sum=self.sum_edges(start.left,sum)
+            sum=self.sum_edges(start.right,sum)
+        return sum
     
 
 
@@ -63,23 +86,34 @@ bt.root.left.left=node(1)
 bt.root.left.right=node(2)
 bt.root.right.left=node(6)
 bt.root.right.right=node(7)
+bt.root.right.left.left=node(8)
+bt.root.right.right.right=node(9)
 
-#                5
-#           /         \
-#          3           4
-#       /     \     /     \
-#      1       2   6       7
+#                 5
+#             /      \
+#           /          \
+#          3             4
+#       /     \       /     \
+#      1       2     6       7
+#                   /  \ 
+#                  8    9
 
+#Preorder:5-3-1-2-4-6-8-7-9-
 
-#Preorder:5-3-1-2-4-6-7-
+#Inoreder:1-3-2-5-8-6-4-7-9-
 
-#Inoreder:1-3-2-5-6-4-7-
+#Postorder:1-2-3-8-6-9-7-4-5-
 
-#Postorder:1-2-3-6-7-4-5-
+#Levelorder:5-3-4-1-2-6-7-8-9-
 
-#Levelorder:5-3-4-1-2-6-7-
+#Reverseorder:9-8-7-6-2-1-4-3-5-
+
+#sum:45
 
 print(bt.print_("Preorder"))
 print(bt.print_("Inorder"))
 print(bt.print_("Postorder"))
 print(bt.print_("Levelorder"))
+print(bt.print_("ReverseLevelOrder"))
+print(bt.print_("SumOfEdges"))
+
